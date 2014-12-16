b455b520dab1561eba885dfd94afbe7188506ed8
Mathias2800 <mathias@valling.net>
2014-12-16 11:03:57 +0100

    Bank 3 translator

---
+++ src/bankthreetranslator/BankThreeTranslator.java
@@ -0,0 +1,67 @@
+
+package bankthreetranslator;
+
+
+import com.rabbitmq.client.AMQP.BasicProperties;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.QueueingConsumer;
+import dk.cphbusiness.connection.ConnectionCreator;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import org.w3c.dom.Document;
+import utilities.xml.xmlMapper;
+
+/**
+ *
+ * @author Mathias
+ */
+public class BankThreeTranslator {
+
+    private static final String BANKEXCHANGE_NAME = "cphbusiness.bankXML";
+    private static final String REPLY_QUEUE = "bank_three_normalizer";
+    private static final String EXCHANGE_NAME = "translator_exchange_topic";
+    private static final String QUEUE_NAME = "bank_three_translator";
+    private static final String[] TOPICS = {"cheap.*" , "expensive.*"};
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ConnectionCreator creator = ConnectionCreator.getInstance();
+        Channel channelIn = creator.createChannel();
+        Channel channelOut = creator.createChannel();
+        channelIn.queueDeclare(QUEUE_NAME, true, false, false, null);
+        channelIn.exchangeDeclare(EXCHANGE_NAME, "topic");
+
+        for (String topic : TOPICS) {
+            channelIn.queueBind(QUEUE_NAME, EXCHANGE_NAME, topic);
+        }
+
+        QueueingConsumer consumer = new QueueingConsumer(channelIn);
+        channelIn.basicConsume(QUEUE_NAME, true, consumer);
+
+
+        while (true) {
+            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+            String message = translateMessage(delivery);
+            System.out.println(message);
+            BasicProperties probs = new BasicProperties.Builder().replyTo(REPLY_QUEUE).correlationId("1").build();
+            channelOut.basicPublish(BANKEXCHANGE_NAME, "", probs, message.getBytes());
+        }
+    }
+
+    private static String translateMessage(QueueingConsumer.Delivery delivery) {
+        String message = new String(delivery.getBody());
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        Document doc = xmlMapper.getXMLDocument(message);
+        try {
+            String ssn = xPath.compile("/LoanRequest/ssn").evaluate(doc);
+            ssn = ssn.replace("-", "");
+            doc.getElementsByTagName("ssn").item(0).getFirstChild().setNodeValue(ssn);
+        } catch (XPathExpressionException ex) {
+            Logger.getLogger(BankThreeTranslator.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return xmlMapper.getStringFromDoc(doc);
+    }
+}
