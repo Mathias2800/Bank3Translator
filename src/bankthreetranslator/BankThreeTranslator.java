f4f6f18a64566f0a94a8e8c0bd5bed7ef85c84b6
Kasper <Kasper@10.50.137.85>
2014-12-17 09:12:41 +0100

    working build

--- src/bankthreetranslator/BankThreeTranslator.java
+++ src/bankthreetranslator/BankThreeTranslator.java
@@ -22,9 +22,9 @@ import utilities.xml.xmlMapper;
 public class BankThreeTranslator {
 
     private static final String BANKEXCHANGE_NAME = "cphbusiness.bankXML";
-    private static final String REPLY_QUEUE = "bank_three_normalizer";
-    private static final String EXCHANGE_NAME = "translator_exchange_topic";
-    private static final String QUEUE_NAME = "bank_three_translator";
+    private static final String REPLY_QUEUE = "bank_three_normalizer_gr1";
+    private static final String EXCHANGE_NAME = "ex_translators_gr1";
+    private static final String QUEUE_NAME = "bank_three_translator_gr1";
     private static final String[] TOPICS = {"cheap.*" , "expensive.*"};
 
     public static void main(String[] args) throws IOException, InterruptedException {
